c166453f6147916a053bd125f6f06009d9b4c5a3
Tanaya <tanayakanerkar@gmail.com>
2024-10-04 15:13:21 +0530

    video changes

--- frontend-client/src/App.js
+++ frontend-client/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar'; // Fixed case-sensitive import
 import UploadVideo from './components/UploadVideo';
-import UploadedVideos from './components/UploadedVideos'; // Removed the extra semicolon
-
+import UploadedVideos from './components/UploadedVideos'; // Ensure this file exists and matches the name
+import Home from "./components/Home";
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
+      <Route path="/" element={<Home />} />
         <Route path="/upload-video" element={<UploadVideo />} />
         <Route path="/all-uploaded-videos" element={<UploadedVideos />} />
       </Routes>
